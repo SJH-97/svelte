30940aeb5d22976756604a5ac1107914e0c2df69
Samuel Harrison <sam.harrison@deptagency.com>
2022-08-27 17:56:16 +0100

    condtional statements

--- src/App.svelte
+++ src/App.svelte
@@ -10,12 +10,28 @@
 		// delete a dog from dogs
 		dogs = dogs.filter((dog) => dog.id !== id);
 	}
+
+	let num = 21;
 </script>
+
+<!-- If Else statement with catch at bottom -->
+
+{#if num > 20}
+	<p>greater than 20</p>
+{:else if num > 5}
+	<p>greater than 5</p>
+{:else} 
+	<p>Not greater than 20 or 5</p>
+{/if}
 
 <main>
  {#each dogs as dog (dog.id)}
 <div>
 	<h4>{dog.name}</h4>
+	<!-- if statement -->
+	{#if dog.breed === 'French Bulldog'}
+		<p>French bulldogs are the best</p>
+	{/if}
 	<p>{dog.age} years old, {dog.breed}</p>
 	<!-- Inline handler -->
 	<button on:click={() => handleClick(dog.id)}>
